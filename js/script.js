2908e3d98406b40de95c4262460cb4a22ec51afe
thibaultMA <thibault.m-a@outlook.com>
2022-03-18 11:18:13 +0100

    v0.5

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-const url = "http://www.omdbapi.com/?s"
-const APIKEY=  "&apikey=afa3ef9"
-const pour = document.querySelector('.bulb')
+const url = "http://www.omdbapi.com/?s";
+const APIKEY=  "&apikey=afa3ef9";
+let pour = document.querySelector('.bulb');
 
-fetch(url+"?s=spider man&apikey=f6e256e1")
-// fetch("http://www.omdbapi.com/?t=spider+man&apikey=f6e256e1")
-.then(res=>res.json())
-.then(data=>console.log(data))
+console.log(pour);
+// fetch(url+"?s=spider man&apikey=f6e256e1")
+// // fetch("http://www.omdbapi.com/?t=spider+man&apikey=f6e256e1")
+// .then(res=>res.json())
+// .then(data=>console.log(data))
 
 
 class t {
@@ -18,16 +19,15 @@ class t {
     }
     html(){
         let h2 = document.createElement('h2')
-        
         let p = document.createElement('p')
         let block = document.createElement('div')
         h2.innerText =this.nom
         p.innerText =this.color
-        block.append(h2)
-        block.append(p)
+        block.appendChild(h2)
+        block.appendChild(p)
         return block
     }
 }
 let tbob = new t('bob',"red")
-
-console.log(pour);
+let fich=tbob.html()
+pour.append(fich)
