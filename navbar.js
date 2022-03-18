6760a696ad7d8d5814164ecaad4dc956f9b26f37
KevinLeNozer <kevin.lenozer@gmail.com>
2022-03-18 15:35:18 +0100

    modif css/Js

--- navbar.js
+++ navbar.js
@@ -1,11 +1,14 @@
-let btn = document.querySelector('#btn');
+let btnBurger = document.querySelector('#btn');
 let sidebar = document.querySelector('.sidebar');
 let searchBtn = document.querySelector('.bx-search');
 
-btn.onclick = function () {
+btnBurger.onclick = function () {
     sidebar.classList.toggle("active");
 }
 
 searchBtn.onclick = function () {
     sidebar.classList.toggle("active");
 }
+
+let paginationLeft = document.querySelector(".nextLeft");
+let paginationRight = document.querySelector("nextRight");
