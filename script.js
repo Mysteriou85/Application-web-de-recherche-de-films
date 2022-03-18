a13202e0fa8bb0919385a300839c8373878b6e78
AlshamiWasin <waseem.fr1999@gmail.com>
2022-03-18 17:19:31 +0100

    V0 2 de mohammad

--- script.js
+++ script.js
@@ -1,40 +1,17 @@
 const url = "http://www.omdbapi.com/?s=";
-const APIKEY = "&apikey=afa3ef9";
+const APIKEY = "&apikey=afa3ef9&plot=full";
 let movieslist = document.querySelector('#movieslist');
 let body = document.querySelector('body');
 let recherche = document.querySelector('input')
 let searchbtn = document.querySelector("button[id='Searchbutton']")
 
-function gettopmovies(numberofmovies) {
-    fetch(url + recherche.value + APIKEY)
-        .then(res => res.json())
-        .then(data => {
-
-
-            data.Search.forEach(movie => {
-                console.log(movie);
-                let t = new Movie(
-                    movie.Title,
-                    movie.Poster,
-                    movie.Type,
-                    movie.Year,
-                    movie.imdbID
-                )
-                movieslist.appendChild(t.html())
-                console.log(movieslist);
-            });
-        })
-        .catch(err => console.log({ message: err }))
-}
-
-
-
 class Movie {
-    constructor(Title, Poster, Type, Year, imdbID) {
+    constructor(Title, Poster, Type, Year, Metascore, imdbID) {
         this.Title = Title
         this.img = Poster
         this.Type = Type
         this.Year = Year
+        this.Metascore = Metascore
         this.imdbID = imdbID
     }
     html() {
@@ -42,7 +19,8 @@ class Movie {
 
         let h2 = document.createElement('h2')
         let img = document.createElement('img')
-        let imgcontainer = document.createElement('div')
+        let moviecontent = document.createElement('div')
+        let movietext = document.createElement('div')
         let Type = document.createElement('p')
         let Year = document.createElement('p')
         let imdbID = document.createElement('a')
@@ -51,35 +29,46 @@ class Movie {
         img.src = this.img
         Type.innerText = this.Type
         Year.innerText = this.Year
+        Metascore.innerText = this.Metascore
         imdbID.href = this.imdbID
 
 
-        imgcontainer.appendChild(img)
-        imgcontainer.appendChild(h2)
-        imgcontainer.appendChild(Type)
-        imgcontainer.appendChild(Year)
-        imdbID.appendChild(imgcontainer)
+
+        moviecontent.appendChild(img)
+        movietext.appendChild(h2)
+        movietext.appendChild(Year)
+        imdbID.appendChild(moviecontent)
+        imdbID.appendChild(movietext)
 
         block.appendChild(imdbID)
 
-        //add class for block
+
+
+        moviecontent.style.backgroundImage = "url(" + img.src + ")"
+        moviecontent.style.backgroundRepeat = "no-repeat";
+        moviecontent.style.backgroundSize = "100% 100%";
+
+        movietext.classList.add("moviestextcontainer")
+            //add class for block
 
         block.classList.add("moviesection")
         img.classList.add("moviesposter")
-        imgcontainer.classList.add("moviesimagecontainer")
+        moviecontent.classList.add("moviesimagecontainer")
 
         return block
     }
 }
 
 searchbtn.addEventListener('click', () => {
+    console.log(url + recherche.value + APIKEY);
     fetch(url + recherche.value + APIKEY)
         .then(res => res.json())
         .then(data => {
 
 
             data.Search.forEach(movie => {
-                console.log(movie);
+                console.log(data);
+                // console.log(movie);
                 let t = new Movie(
                     movie.Title,
                     movie.Poster,
@@ -88,7 +77,6 @@ searchbtn.addEventListener('click', () => {
                     movie.imdbID
                 )
                 movieslist.appendChild(t.html())
-                console.log(movieslist);
             });
         })
         .catch(err => console.log({ message: err }))
