ca2a12a77aa074788644b9b7199c4867fc15a2ce
Mysteriou85 <guib5@hotmail.fr>
2022-03-22 10:16:10 +0100

    update

--- js/scriptpagefilm.js
+++ js/scriptpagefilm.js
@@ -1,32 +1,42 @@
-const url = "http://www.omdbapi.com/?i=";
+const url = "https://www.omdbapi.com/?i=";
 const APIKEY = "&apikey=afa3ef9";
-//let resultatRecherche = document.querySelector('.resultatRecherche');
-let recherche = localStorage.getItem('movie');
-//let recherche = 'tt2975590';
-//document.querySelector('input')
-//let btn = document.querySelector('button')
+let resultatRecherche = localStorage.getItem("movie");
+let Searchbutton = document.querySelector("#Searchbutton");
+let inputRecherche = document.querySelector("#inputSearch");
 
-fetch(url + recherche + APIKEY + '&plot=full')
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        document.getElementById("titreFilm").innerHTML = data.Title;
-        document.getElementById("runtimeFilm").innerHTML = data.Runtime;
-        document.getElementById("releasedFilm").innerHTML = data.Released;
-        document.getElementById("ratedFilm").innerHTML = data.Rated;
-        document.getElementById("genreFilm").innerHTML = data.Genre;
-        document.getElementById("directorFilm").innerHTML = data.Director;
-        document.getElementById("writerFilm").innerHTML = data.Writer;
-        document.getElementById("actorsFilm").innerHTML = data.Actors;
-        document.getElementById("plotFilm").innerHTML = data.Plot;
-        document.getElementById("countryFilm").innerHTML = data.Country;
-        document.getElementById("awardsFilm").innerHTML = data.Awards;
+inputRecherche.addEventListener("keyup", function (e) {
+  console.log("do");
+  if (e.keyCode === 13) {
+    Searchbutton.click();
+  }
+});
 
-        document.getElementById("metascoreFilm").innerHTML = data.Metascore;
+Searchbutton.addEventListener("click", () => {
+  localStorage.setItem("recherche", inputRecherche.value);
+  window.location = "./index.html";
+});
 
-        document.getElementById("imdbRatingFilm").innerHTML = data.imdbRating;
-        document.getElementById("imdbVotesFilm").innerHTML = data.imdbVotes;
+fetch(url + resultatRecherche + APIKEY + "&plot=full")
+  .then((response) => response.json())
+  .then((data) => {
+    document.getElementById("titreFilm").innerHTML = data.Title;
+    document.getElementById("runtimeFilm").innerHTML = data.Runtime;
+    document.getElementById("releasedFilm").innerHTML = data.Released;
+    document.getElementById("ratedFilm").innerHTML = data.Rated;
+    document.getElementById("genreFilm").innerHTML = data.Genre;
+    document.getElementById("directorFilm").innerHTML = data.Director;
+    document.getElementById("writerFilm").innerHTML = data.Writer;
+    document.getElementById("actorsFilm").innerHTML = data.Actors;
+    document.getElementById("plotFilm").innerHTML = data.Plot;
+    document.getElementById("countryFilm").innerHTML = data.Country;
+    document.getElementById("awardsFilm").innerHTML = data.Awards;
 
-        document.getElementById("posterFilm").src = data.Poster;
-    })
-    .catch(err => console.log({ message: err }))
+    document.getElementById("metascoreFilm").innerHTML = data.Metascore;
+
+    document.getElementById("imdbRatingFilm").innerHTML = data.imdbRating;
+    document.getElementById("imdbVotesFilm").innerHTML = data.imdbVotes;
+
+    document.getElementById("posterFilm").src = data.Poster;
+    document.getElementById("posterFilm").alt = data.Title;
+  })
+  .catch((err) => console.log({ message: err }));
